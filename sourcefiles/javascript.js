486d800516f35afa05152a8d9bacd53689cd9362
Jennifer Kuhn <63382464+jkdoesntcare@users.noreply.github.com>
2022-07-21 16:58:40 -0400

    added weather api

--- sourcefiles/javascript.js
+++ sourcefiles/javascript.js
@@ -1,1 +1,17 @@
+function displayTemperture(response) {
+  console.log(response.data);
+  let temperture = document.querySelector("#searchTemp");
+  let city = document.querySelector("#searchCity");
+  let humidity = document.querySelector("#humidity");
+  let windSpeed = document.querySelector("#wind");
+  temperture.innerHTML = Math.round(response.data.main.temp);
+  city.innerHTML = response.data.name;
+  humidity.innerHTML = `Humidity: ${response.data.main.humidity}`;
+  windSpeed.innerHTML = `Wind: ${response.data.wind.speed}km/h`;
+}
 
+let apiKey = "ee765fe52026bce2fd4a1b8bc1e42692";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperture);
